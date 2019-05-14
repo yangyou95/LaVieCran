68f909078d8dace473517b3a83c1cddb90b78111
Yang <bboylevi89@gmail.com>
2019-05-14 20:44:07 +0200

    change navbar

--- resources/views/navbar_header.blade.php
+++ resources/views/navbar_header.blade.php
@@ -1,6 +1,8 @@
+
+
 <div class="navbar-header">
 
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-top">
 
         <span class="sr-only">Toggle navigation</span>
 
@@ -20,176 +22,29 @@
 
 
 
-
-
-    <ul class="nav navbar-top-links navbar-right collapse in" id="navbar-collapse">
-
-
-        <li class="dropdown">
-
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-
-                <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i>
-
-            </a>
-
-            <ul class="dropdown-menu dropdown-messages">
-
-                <li>
-
-                    <a href="#">
-
-                        <div>
-
-                            <strong>站内信系统</strong>
-
-                            <span class="pull-right text-muted">
-
-                                <em>Yesterday</em>
-
-                            </span>
-
-                        </div>
-
-                        <div>站内信系统。Yang正在建设中...</div>
-
-                    </a>
-
-                </li>
-
-                <li class="divider"></li>
-
-                <li>
-
-                    <a class="text-center" href="#">
-
-                        <strong>Read All Messages</strong>
-
-                        <i class="fa fa-angle-right"></i>
-
-                    </a>
-
-                </li>
-
-            </ul>
-
-            <!-- /.dropdown-messages -->
-
-        </li>
-
-        <!-- /.dropdown -->
-
-        <li class="dropdown">
-
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-
-                <i class="fa fa-tasks fa-fw"></i> <i class="fa fa-caret-down"></i>
-
-            </a>
-
-            <ul class="dropdown-menu dropdown-tasks">
-
-                <li>
-
-                    <a href="#">
-
-                        <div>
-
-                            <p>
-
-                                <strong>项目进度</strong>
-
-                                <span class="pull-right text-muted">70% Complete</span>
-
-                            </p>
-
-                            <div class="progress progress-striped active">
-
-                                <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
-
-                                    <span class="sr-only">70% Complete (danger)</span>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </a>
-
-                </li>
-
-                <li class="divider"></li>
-
-                <li>
-
-                    <a class="text-center" href="#">
-
-                        <strong>所有项目(校内项目系统，Yang建设中)</strong>
-
-                        <i class="fa fa-angle-right"></i>
-
-                    </a>
-
-                </li>
-
-            </ul>
-
-            <!-- /.dropdown-tasks -->
-
-        </li>
-
-        <!-- /.dropdown -->
-
-        <li class="dropdown">
-
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-
-                <i class="fa fa-bell fa-fw"></i> <i class="fa fa-caret-down"></i>
-
-            </a>
-
-            <ul class="dropdown-menu dropdown-alerts">
-
-                <li>
-
-                    <a href="#">
-
-                        <div>
-
-                            <i class="fa fa-upload fa-fw"></i> Server Rebooted
-
-                            <span class="pull-right text-muted small">4 minutes ago</span>
-
-                        </div>
-
-                    </a>
-
-                </li>
-
-                <li class="divider"></li>
-
-                <li>
-
-                    <a class="text-center" href="#">
-
-                        <strong>查看通知</strong>
-
-                        <i class="fa fa-angle-right"></i>
-
-                    </a>
-
-                </li>
-
-            </ul>
-
-            <!-- /.dropdown-alerts -->
-
-        </li>
-
-        <!-- /.dropdown -->
-
-
-
-
-    </ul>
+      <ul class="nav navbar-top-links navbar-right collapse in" id="navbar-top">
+
+    <li>
+        <a href="/items">
+            二手物品
+        </a>
+    </li>
+
+    <li>
+        <a href="/items">
+            二手物品
+        </a>
+    </li>
+
+    <li>
+        <a href="/items">
+            二手物品
+        </a>
+    </li>
+
+    <li>
+        <a href="/items">
+            二手物品
+        </a>
+    </li>
+</ul>
