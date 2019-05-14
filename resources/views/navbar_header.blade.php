4db5ae49a3e944c9d6eb6fda852028fac9754cc5
Yang <bboylevi89@gmail.com>
2019-05-14 20:10:51 +0200

    change navbar

--- resources/views/navbar_header.blade.php
+++ resources/views/navbar_header.blade.php
@@ -1,3 +1,5 @@
+<div class="container">
+
 <div class="navbar-header">
 
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-top">
@@ -50,4 +52,5 @@
 
 
 </ul>
+</div>
 </div>
