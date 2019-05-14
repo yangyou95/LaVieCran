88f96dc66b9e98fce7742e5f2354e8d48afa8a4c
Yang <bboylevi89@gmail.com>
2019-05-14 17:38:39 +0200

    change navbar

--- resources/views/navbar_header.blade.php
+++ resources/views/navbar_header.blade.php
@@ -24,158 +24,50 @@
 
         <a class="dropdown-toggle" data-toggle="dropdown" href="/items">
 
-            <i class="fas fa-shopping-basket"></i> <i class="fa fa-caret-down"></i>
+
             二手物品
         </a>
-
-        <ul class="dropdown-menu dropdown-messages">
-
-
-
-            <li class="divider"></li>
-
-            <li>
-
-                <a class="text-center" href="#">
-
-                    <strong>Read All Messages</strong>
-
-                    <i class="fa fa-angle-right"></i>
-
-                </a>
-
-            </li>
-
-        </ul>
 
         <!-- /.dropdown-messages -->
 
     </li>
 
-    <!-- /.dropdown -->
-
     <li class="dropdown">
 
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="/items">
 
-            <i class="fa fa-tasks fa-fw"></i> <i class="fa fa-caret-down"></i>
 
+            求购物品
         </a>
 
-        <ul class="dropdown-menu dropdown-tasks">
-
-            <li>
-
-                <a href="#">
-
-                    <div>
-
-                        <p>
-
-                            <strong>项目进度</strong>
-
-                            <span class="pull-right text-muted">70% Complete</span>
-
-                        </p>
-
-                        <div class="progress progress-striped active">
-
-                            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
-
-                                <span class="sr-only">70% Complete (danger)</span>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </a>
-
-            </li>
-
-            <li class="divider"></li>
-
-            <li>
-
-                <a class="text-center" href="#">
-
-                    <strong>所有项目(校内项目系统，Yang建设中)</strong>
-
-                    <i class="fa fa-angle-right"></i>
-
-                </a>
-
-            </li>
-
-        </ul>
-
-        <!-- /.dropdown-tasks -->
+        <!-- /.dropdown-messages -->
 
     </li>
 
-    <!-- /.dropdown -->
-
     <li class="dropdown">
 
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="/items">
 
-            <i class="fa fa-bell fa-fw"></i> <i class="fa fa-caret-down"></i>
 
+            二手物品
         </a>
 
-        <ul class="dropdown-menu dropdown-alerts">
-
-            <li>
-
-                <a href="#">
-
-                    <div>
-
-                        <i class="fa fa-upload fa-fw"></i> Server Rebooted
-
-                        <span class="pull-right text-muted small">4 minutes ago</span>
-
-                    </div>
-
-                </a>
-
-            </li>
-
-            <li class="divider"></li>
-
-            <li>
-
-                <a class="text-center" href="#">
-
-                    <strong>查看通知</strong>
-
-                    <i class="fa fa-angle-right"></i>
-
-                </a>
-
-            </li>
-
-        </ul>
-
-        <!-- /.dropdown-alerts -->
+        <!-- /.dropdown-messages -->
 
     </li>
 
-    <!-- /.dropdown -->
-
     <li class="dropdown">
 
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="/items">
 
-            <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
 
+            二手物品
         </a>
 
-
+        <!-- /.dropdown-messages -->
 
     </li>
 
-    <!-- /.dropdown -->
+
 
 </ul>
