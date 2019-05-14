4d782a9d6e34867b206b8fa796b98cebe69f9106
Yang <bboylevi89@gmail.com>
2019-05-14 20:30:55 +0200

    change navbar

--- resources/views/navbar_header.blade.php
+++ resources/views/navbar_header.blade.php
@@ -1,8 +1,6 @@
-
-
 <div class="navbar-header">
 
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-top">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 
         <span class="sr-only">Toggle navigation</span>
 
