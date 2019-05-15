0e6a30604332ef2f88ff931dce832754294d66a5
Yang <bboylevi89@gmail.com>
2019-05-16 01:10:45 +0200

    change navbar

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -14,6 +14,25 @@
 
   <link href="{!! asset('usercenter/vendor/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
 
+  <!-- MetisMenu CSS -->
+
+  <link href="{!! asset('usercenter/vendor/metisMenu/metisMenu.min.css') !!}" rel="stylesheet">
+
+
+
+  <!-- Custom CSS -->
+
+  <link href="{!! asset('usercenter/dist/css/sb-admin-2.css') !!}" rel="stylesheet">
+
+
+
+  <!-- Morris Charts CSS -->
+
+  <link href="{!! asset('usercenter/vendor/morrisjs/morris.css') !!}" rel="stylesheet">
+
+
+
+
   <!-- Custom Fonts -->
 
   <link href="{!! asset('usercenter/vendor/font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css">
