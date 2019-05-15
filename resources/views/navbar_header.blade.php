48fc178fcc89bb620d09bba8dfb87b7d49aeeed4
Yang <bboylevi89@gmail.com>
2019-05-16 00:21:37 +0200

    change navbar

--- resources/views/navbar_header.blade.php
+++ resources/views/navbar_header.blade.php
@@ -21,30 +21,35 @@
 
       <ul class="nav collapse in" id="navbar-top">
 
-    <li class="nav-item">
+    <li>
         <a href="/items">
             二手物品
         </a>
     </li>
 
-    <li class="nav-item">
-        <a href="/items">
-            二手物品
+    <li>
+        <a href="/buy">
+            求购信息
+        </a>
+    </li>
+  @if (Route::has('login'))
+    <li>
+        <a href="{{ route('login') }}">
+            登录
         </a>
     </li>
 
-    <li class="nav-item">
-        <a href="/items">
-            二手物品
+    <li>
+        <a href="{ route('register') }}">
+            注册
         </a>
     </li>
-
-    <li class="nav-item">
-        <a href="/items">
-            二手物品
-        </a>
-    </li>
-
+@else
+<li>
+    <a href="{{ url('/home') }}">
+        登录
+    </a>
+</li>
 
 
 
