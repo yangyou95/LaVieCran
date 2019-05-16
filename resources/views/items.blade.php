698b4753519489cc19c3af3fb6cc1b230ed7a8b1
Yang <bboylevi89@gmail.com>
2019-05-16 23:45:17 +0200

    change filestorage

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -216,7 +216,7 @@
                            <img src= "{{$d->path}}" alt="180x100%" style="height: 120px ;width: 100%;display: block";>
                           </div>
                           <div class="caption">
-                              <h5>{{$d->title}}</h5>
+                              <p>{{$d->title}}</p>
                               <p>{{$d->price}}</p>
                               <p><small class="text-muted">{{$d->created_at}}</small></p>
 
