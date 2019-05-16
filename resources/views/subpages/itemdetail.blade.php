accfa6e4aa23244b8c0ec5e406c791c09635cc1e
Yang <bboylevi89@gmail.com>
2019-05-17 00:20:19 +0200

    change filestorage

--- resources/views/subpages/itemdetail.blade.php
+++ resources/views/subpages/itemdetail.blade.php
@@ -109,7 +109,7 @@
      <div class="links">
                     <div class="row">
                     <div class="image_resize col-xs-9">
-                    <img class="col-xs-9" src= "{{$Itemdetail->path}}" alt="180x100%" style="height: auto;width: auto;max-height: 100px;max-width: 100px;";>
+                    <img class="col-xs-9" src= "{{$Itemdetail->path}}" alt="180x100%" style="height: auto;width: auto;max-height: 300px;max-width: 300px;";>
                    </div>
                    </div>
 
